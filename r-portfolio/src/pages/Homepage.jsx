8a43d07eec250d689ccef9e700ae8854f0df99d8
JObengDappah <134343671+JObengDappah@users.noreply.github.com>
2023-11-17 00:05:13 -0500

    home page modification

--- r-portfolio/src/pages/Homepage.jsx
+++ r-portfolio/src/pages/Homepage.jsx
@@ -7,7 +7,7 @@ function Homepage() {
         <div className='container text-center mt-3 aboutContainer'>
             <h2 className=''>About Me</h2>
             <div>
-                <img src= '/R-Portfolio/r-portfolio/public/images/nana-modified.png' alt='Profile picture' className='img-thumbnail img-fluid proPic m-3'></img>
+                <img src= '/R-Portfolio/r-portfolio/public/images/nana-modified.png' alt='Profile picture'></img>
             </div>
             <div>
                 <p>Hello Wolrd! My name is Jason Obeng Dappah, and my journey to become a fullstack web developer for 6 months has thought me a lot of skills required to excel at this career path, Skills such as Html/Css/Javascript/Bootstrap/React/Jquery/...</p>
